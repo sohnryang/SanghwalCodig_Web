89a33d967f3c3521302693489c3600bcb9ff7aea
sohnryang <ryang030528@gmail.com>
2017-01-08 19:06:37 +0900

    added 5.php

---
+++ phpjs/5.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        document.write(2 == 1);
+    </script>
+    <h1>PHP</h1>
+    <?php
+        var_dump(2 == 1);
+    ?>
+</body>
+</html>
