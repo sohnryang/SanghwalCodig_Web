40ec4aded13734137f6b361525337d152b3839b4
sohnryang <ryang030528@gmail.com>
2017-01-08 11:35:53 +0900

    added 4.php

---
+++ phpjs/4.php
@@ -0,0 +1,19 @@
+<?php
+    $name = "이고잉";
+?>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        name = "egoing";
+        document.write("안녕하세요, " + name);
+    </script>
+    <h1>PHP</h1>
+    <?php
+        echo "안녕하세요, " . $name;
+    ?>
+</body>
+</html>
