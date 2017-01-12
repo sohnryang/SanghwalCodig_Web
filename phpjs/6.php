a364dd879a742e4d3c2fdb0cf08c6521c51991b4
sohnryang <ryang030528@gmail.com>
2017-01-13 08:38:55 +0900

    added 6.php

---
+++ phpjs/6.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        result = (1 == 2);
+        if(result) {
+            document.write("참");
+        } else {
+            document.write("거짓");
+        }
+    </script>
+    <h1>PHP</h1>
+    <?php
+        $result = (1 == 2);
+        if ($result) {
+            echo "참";
+        } else {
+            echo "거짓";
+        }
+    ?>
+</body>
+</html>
