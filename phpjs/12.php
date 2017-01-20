044ae9735a848400074e7411b67b37d9820fbe03
sohnryang <ryang030528@gmail.com>
2017-01-21 08:41:48 +0900

    added 12.php

---
+++ phpjs/12.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>JavaScript</h1>
+    <script>
+        function a() {
+            document.write("Hello JS Function");
+        }
+        a();
+    </script>
+
+    <h1>PHP</h1>
+    <?php
+        function a() {
+            echo "Hello PHP Function";
+        }
+        a();
+    ?>
+</body>
+</html>
