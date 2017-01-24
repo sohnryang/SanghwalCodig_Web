4acc2c64642fb15b84b3221249337adda6b714df
sohnryang <ryang030528@gmail.com>
2017-01-24 17:27:39 +0900

    changed DB read

--- index.php
+++ index.php
@@ -28,11 +28,13 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
     </div>
     <article>
         <?php
-            $sql = 'SELECT * FROM topic WHERE id='.$_GET['id'];
-            $result = mysqli_query($conn, $sql);
-            $row = mysqli_fetch_assoc($result);
-            echo '<h2>'.$row['title'].'</h2>';
-            echo $row['description'];
+            if(!empty($_GET['id'])) {
+                $sql = 'SELECT * FROM topic WHERE id='.$_GET['id'];
+                $result = mysqli_query($conn, $sql);
+                $row = mysqli_fetch_assoc($result);
+                echo '<h2>'.$row['title'].'</h2>';
+                echo $row['description'];
+            }
         ?>
     </article>
 </body>
