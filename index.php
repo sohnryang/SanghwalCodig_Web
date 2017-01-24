0adfe990df7608e7ecd14f5343acee9f49f90ab4
sohnryang <ryang030528@gmail.com>
2017-01-24 21:31:28 +0900

    added multi-user support

--- index.php
+++ index.php
@@ -30,10 +30,11 @@
     <article>
         <?php
             if(!empty($_GET['id'])) {
-                $sql = 'SELECT * FROM topic WHERE id='.$_GET['id'];
+                $sql = "SELECT topic.id,title,name,description FROM topic LEFT JOIN user ON topic.author = user.id WHERE topic.id=".$_GET['id'];
                 $result = mysqli_query($conn, $sql);
                 $row = mysqli_fetch_assoc($result);
                 echo '<h2>'.$row['title'].'</h2>';
+                echo '<p>'.$row['name'].'</p>';
                 echo $row['description'];
             }
         ?>
