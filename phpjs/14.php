42d94eb0bfdc029df8e8b255c5458c94b463fa87
sohnryang <ryang030528@gmail.com>
2017-01-26 19:12:29 +0900

    fixed SQL injection vulnerability

--- phpjs/14.php
+++ phpjs/14.php
@@ -1,6 +1,8 @@
 <?php
     $conn = mysqli_connect("localhost", "root", "P4ssw0rd_T3st", "opentutorials");
-    $sql = "SELECT * FROM user WHERE name='".$_GET['name']."' AND password='".$_GET['password']."'";
+    $name = mysqli_real_escape_string($conn, $_GET['name']);
+    $password = mysqli_real_escape_string($conn, $_GET['password']);
+    $sql = "SELECT * FROM user WHERE name='".$name."' AND password='".$password."'";
     echo $sql."<br/>";
     $result = mysqli_query($conn, $sql);
     var_dump($result);
@@ -12,10 +14,10 @@
 </head>
 <body>
     <?php
-        if ($result->num_rows == "1") {
-            echo "주인님 환영합니다.";
-        } else {
+        if ($result->num_rows == "0") {
             echo "뉘신지?";
+        } else {
+            echo "주인님 환영합니다.";
         }
     ?>
 </body>
